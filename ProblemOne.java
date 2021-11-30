740ec3d2990d34424aa4bec932de6b7c545c9aa2
Abhi712-ui <53882305+Abhi712-ui@users.noreply.github.com>
2021-11-30 10:33:50 -0800

    message

--- ProblemOne.java
+++ ProblemOne.java
@@ -2,4 +2,13 @@ class ProblemOne{
 public static void main(String[] args){
     
 }
+public static int lastIndexOf(int[] a, int b){
+int c = -1;
+for(int d = 0; d <= a.length - 1; d++){
+if(a[d] == b){
+    c = d;
+}
+}
+return c;
+}
 }
