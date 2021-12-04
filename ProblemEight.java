85bf4402737ff43e72368ebd13c8e7e89f977947
Abhimanyu Sharma <abhisea12@gmail.com>
2021-12-03 19:29:55 -0800

    VScode kinda Sus

--- ProblemEight.java
+++ ProblemEight.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 class ProblemEight{
     public static void main(String[] args){
-    
+    int[] Values = {6, 2, 4, 17, 55, 4, 3, 26, 18, 2, 17};
+    System.out.println(median(Values));
+}
+
+public static int median(int[] listOfValues){
+    int len = listOfValues.length;
+    Arrays.sort(listOfValues);
+    int There = (len%2);
+    int Median = (len - There)/2;
+    return listOfValues[Median];
 }
 }
--- ProblemTen.java
+++ ProblemTen.java
@@ -1,5 +1,17 @@
 class ProblemTen{
     public static void main(String[] args){
-    
+    int[] p = {2, 2, 2, 5, 7, 6, 5};
+    System.out.println(percentEven(p));
+}
+
+public static double percentEven(int[] vals){
+    int even = 0;
+    for(int i = 0; i < vals.length; i++){
+        if(vals[i] % 2 == 0){
+        even++;
+        }
+    }  
+  double percent = (even / vals.length) *100;
+  return percent;
 }
 }
